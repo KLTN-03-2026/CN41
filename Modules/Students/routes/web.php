91b5dff3d8ffb6a9e7dd683cd233805f7ad303b7
Ahryxx <phvanthanh06@gmail.com>
2026-04-09 16:39:14 +0700

    chore: remove project configuration and dependency files

--- Modules/Students/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Students\Http\Controllers\StudentsController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('students', StudentsController::class)->names('students');
-});
