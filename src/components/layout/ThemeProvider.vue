d21bc14476f50975d906bc6259f1958ae725938f
Ahryxx <phvanthanh06@gmail.com>
2026-04-09 16:39:14 +0700

    chore: remove project configuration and dependency files

--- src/components/layout/ThemeProvider.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <slot></slot>
-</template>
-
-<script setup lang="ts">
-import { useThemeProvider } from '@/composables/useTheme'
-
-useThemeProvider()
-</script>
--- src/plugins/nprogress.js
+++
@@ -1,5 +0,0 @@
-import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
-
-NProgress.configure({ showSpinner: false, speed: 400 })
-export default NProgress
